4c5bc001d5bc25278f5a0f1d6e800ff05eac6de3
ja-brouil <james_brouillet@hotmail.com>
2017-05-05 19:21:44 -0700

    Update Add Favorite Servers.sh

--- Add Favorite Servers.sh
+++ Add Favorite Servers.sh
@@ -3,9 +3,9 @@
 # Written by James Brouillet
 # Variables
 
-QUMULO="smb://qumulo"
-OLDSERVERAFP="afp://10.1.115.2"
-OLDSERVERSMB="smb://10.1.115.2"
+QUMULO="URL://SERVERNAME"
+OLDSERVERAFP="URL://SERVERNAME"
+OLDSERVERSMB="URL://SERVERNAME"
 #this check if the names are already in there
 CHECK1=$(/usr/bin/sfltool dump-storage ~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.FavoriteServers.sfl | awk '{print $NF}' | sort -u | sed -ne 's/^URL://p')
 #Clear Terminal screen
@@ -16,14 +16,14 @@ echo "Removing old server favorite files."
 sfltool clear
 killall Finder
 
-echo "Adding Qumulo"
-/usr/bin/sfltool add-item -n "Qumulo" com.apple.LSSharedFileList.FavoriteServers "smb://qumulo"
+echo "Adding SERVERNAME"
+/usr/bin/sfltool add-item -n "Servername" com.apple.LSSharedFileList.FavoriteServers "URL://SERVERNAME"
 
 echo -e "\nAdding Old Server"
-/usr/bin/sfltool add-item -n "Old Server AFP" com.apple.LSSharedFileList.FavoriteServers "afp://10.1.115.2"
+/usr/bin/sfltool add-item -n "Old Server AFP" com.apple.LSSharedFileList.FavoriteServers "URL://SERVERNAME"
 
 echo -e "\nAdding Old Server SMB"
-/usr/bin/sfltool add-item -n "Old Server SMB" com.apple.LSSharedFileList.FavoriteServers "afp://10.1.115.2"
+/usr/bin/sfltool add-item -n "Old Server SMB" com.apple.LSSharedFileList.FavoriteServers "URL://SERVERNAME"
 
 killall cfpresfsd
 killall Finder
